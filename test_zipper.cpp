814e9f85fe19d0fd5506dcedf56b2a0df2c70330
Brett Viren <brett.viren@gmail.com>
2021-05-25 17:58:53 -0400

    Some flailing trying to inherit from priority_queue.
    Just say no.

--- test_zipper.cpp
+++ test_zipper.cpp
@@ -3,9 +3,14 @@
 
 using namespace zipper;
 
+using node_t = std::pair<int, int>;
+
+using merged_ints_t = merged_queue< node_t, std::vector<node_t>,
+                                    std::greater<node_t> >;
+
 void test_merge_queue()
 {
-    merged_queue< int, int, std::greater<int> > mq(3);
+    merged_ints_t mq(3);
 
     // 0: 1, 2, 3, 4
     // 1: 2, 4,10
@@ -13,26 +18,26 @@ void test_merge_queue()
 
     mq.push(1, 0);
     assert(!mq.complete());
-    assert(mq.top().element == 1);
+    assert(mq.top().first == 1);
 
     mq.push(2, 1);
     assert(!mq.complete());
-    assert(mq.top().element == 1);
+    assert(mq.top().first == 1);
 
     mq.push(0, 2);
     assert(!mq.complete());
-    assert(mq.top().element == 0);
+    assert(mq.top().first == 0);
 
     mq.push(2, 0);
     assert(!mq.complete());
-    assert(mq.top().element == 0);
+    assert(mq.top().first == 0);
            
     mq.push(4, 1);
     mq.push(3, 2);
     assert(mq.complete());
-    assert(mq.top().element == 0);
+    assert(mq.top().first == 0);
     mq.pop();
-    assert(mq.top().element == 1);
+    assert(mq.top().first == 1);
     assert(mq.complete());
 
     mq.push(3, 0);
@@ -42,36 +47,61 @@ void test_merge_queue()
     mq.push(9,2);
 
     mq.pop();
-    assert(mq.top().element == 2);
+    assert(mq.top().first == 2);
     mq.pop();
-    assert(mq.top().element == 2);
+    assert(mq.top().first == 2);
     mq.pop();
-    assert(mq.top().element == 3);
+    assert(mq.top().first == 3);
     mq.pop();
-    assert(mq.top().element == 3);
+    assert(mq.top().first == 3);
     mq.pop();
-    assert(mq.top().element == 4);
+    assert(mq.top().first == 4);
     mq.pop();
-    assert(mq.top().element == 4);
+    assert(mq.top().first == 4);
     mq.pop();
-    assert(mq.top().element == 5);
-    assert(mq.top().stream == 2);
+    assert(mq.top().first == 5);
+    assert(mq.top().second == 2);
     assert(!mq.complete());
     mq.pop();
-    assert(mq.top().element == 9);
-    assert(mq.top().stream == 2);
+    assert(mq.top().first == 9);
+    assert(mq.top().second == 2);
     mq.pop();
-    assert(mq.top().element == 10);
-    assert(mq.top().stream == 1);
+    assert(mq.top().first == 10);
+    assert(mq.top().second == 1);
     mq.pop();
     
+}
+#include <chrono>
+#include <iostream>
+
+void test_merge_speed()
+{
+    auto t0 = std::chrono::steady_clock::now();
+    merged_ints_t mq(3);
+
+    int nele = 10000000;
+    for (int ele=0; ele<nele; ++ele) {
+        for (int k=0; k<3; ++k) {
+            mq.push(ele,k);
+        }
+        if (mq.size() < 100) {
+            continue;
+        }
+        for (int k=0; k<3; ++k) {
+            mq.push(ele,k);
+        }            
+    }
+    auto t1 = std::chrono::steady_clock::now();
+    double dt = std::chrono::duration_cast<std::chrono::microseconds>(t1-t0).count();
+    double rate = nele / dt;
+    std::cerr << "N="<< nele*1e-6 << " M, " << dt*1e-6 << " s, " << rate << " MHz" << std::endl;
 }
 
 
 int main()
 {
     test_merge_queue();
-
+    test_merge_speed();
 
     return 0;
 }
--- zipper.hpp
+++ zipper.hpp
@@ -6,65 +6,74 @@
 
 namespace zipper {
 
+    // template <typename Element, typename Stream>
+    // struct Node {
+    //     Element element;
+    //     Stream stream;
+    // };
+
+    // template <typename Element, typename Stream>
+    // struct NodeAscending {
+    //     using node_t = Node<Element, Stream>;
+    //     bool operator()(const node_t& a, const node_t& b) {
+    //         return a.element > b.element;
+    //     }
+    // };
+    // template <typename Element, typename Stream>
+    // struct NodeDescending {
+    //     using node_t = Node<Element, Stream>;
+    //     bool operator()(const node_t& a, const node_t& b) {
+    //         return a.element < b.element;
+    //     }
+    // };
+
     /**
-       A k-way merged queue.
+       A k-way merged queue. 
 
-       This implements a max-heap, "larger" elements have priority.
-       Caller should pass Comp = std::greater<Element> if the
-       "smaller" elements should have higher priority.
-     */
-    template <typename Element, typename Stream,
-              typename Compare = std::less<Element>>
-    class merged_queue {
+       A "node" is a pair: (priority, identity)
+
+    */
+    template <typename Node,
+              typename Sequence = std::vector<Node>,
+              typename Compare = std::less<Node> >
+    class merged_queue  {
     public:
+        using node_t = Node;
+        using queue_t = std::priority_queue<Node, Sequence, Compare>;
+        using priority_t = typename Node::first_type;
+        using identity_t = typename Node::second_type;
 
-        struct Node {
-            Element element;
-            Stream stream;
-
-            bool operator<(const Node& rhs) const {
-                Compare comp;
-                return comp(element, rhs.element);
-            }
-        };
-
-        explicit merged_queue (size_t k) : cardinality(k) { }
-
-
-        template <class InputIterator>
-        merged_queue (size_t k,
-                      InputIterator first, InputIterator last)
-            : cardinality(k)
+        explicit merged_queue (size_t k,
+                               const Compare& comp = Compare())
+            : waiting(comp)
+            , cardinality(k)
         {
-            while (first != last) {
-                this->insert(*first);
-                ++first;
-            }
         }
 
         /// Push as Node.
-        void push(const Node& node) {
-            const auto str = node.stream;
+        void push(const node_t& node) {
+            const auto ident = node.second;
             waiting.push(node);
-            occupancy[str] += 1;
+            occupancy[ident] += 1;
+        }
+        void push(const priority_t& pri, const identity_t& ident)
+        {
+            push(node_t(pri, ident));
         }
 
-        /// Push next element from a stream.
-        void push(const Element& ele, const Stream str) {
-            push(Node{ele, str});
+        const node_t& top() const {
+            return waiting.top();
         }
 
         /// Pop the top.
         void pop() {
-            const auto str = top().stream;
+            const auto ident = top().second;
             waiting.pop();
-            occupancy[str] -= 1;
+            occupancy[ident] -= 1;
         }
 
-        /// Access the current top
-        const Node& top() {
-            return waiting.top();
-        }
+        bool empty() const { return waiting.empty(); }
+        size_t size() const { return waiting.size(); }
 
         /**
            Return true if the queue has sufficient representation (at
@@ -72,11 +81,11 @@ namespace zipper {
            pop.  This is an O(k) check.
          */
         bool complete() const {
-            auto str = waiting.top().stream;
+            auto ident = top().second;
             size_t missing = cardinality;
             for (const auto& it : occupancy) {
                 size_t have = it.second;
-                if (it.first == str) {
+                if (it.first == ident) {
                     assert(have > 0);
                     have -= 1;
                 }
@@ -87,18 +96,22 @@ namespace zipper {
             return missing == 0;
         }
 
-        // forward to priority_queue
-        size_t size() const { return waiting.size(); }
-        bool empty() const { return waiting.empty(); }
-
     private:
         
         const size_t cardinality;
-        std::priority_queue<Node> waiting;
-        std::unordered_map<Stream, size_t> occupancy;
+        queue_t waiting;
+        std::unordered_map<identity_t, size_t> occupancy;
     };
 
 
+    
+    // template <typename MQueue, typename Time = time_t>
+    // class tardy_queue {
+    //     MQueue 
+
+    // public:
+    //     tardy_queue(
+    // };
 
 }
 #endif
